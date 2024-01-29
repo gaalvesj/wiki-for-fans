04504a438cc74605c28d28af6f1da089e059dd9c
Gabriel <gabrielalves3654@gmail.com>
2024-01-29 13:01:52 -0300

    add schmema with graphql

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,27 @@
+import "reflect-metadata";
 import path from 'path';
-import { buildSchema } from "graphql";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from 'apollo-server';
+import { UserResolver } from "./src/resolvers/userResolvers";
 
 async function main() {
     const schema = await buildSchema({
-        resolvers: [],
-        emitSchemaFile: path.resolve(, "schema.gpl"),
+        resolvers: [
+            UserResolver,
+        ],
+        emitSchemaFile: path.resolve(__dirname, "schema.gql"),
     })
+    const server = new ApolloServer({
+        schema,
+    })
+
+    const { url } = await server.listen()
+
+    console.log(`The server is on in ${url}`);
 }
 
 main();
+
+function hello() {
+    throw new Error("Function not implemented.");
+}
