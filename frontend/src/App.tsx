c95787f9bcb098593490a1dd02f3dc961d630547
Gabriel <gabrielalves3654@gmail.com>
2024-01-30 12:48:18 -0300

    integration front and back done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,39 @@
+import { gql, useQuery } from '@apollo/client'
 import './App.css'
 
+type  Character = {
+  id: string;
+  name: string;
+  age: number;
+  anime: string;
+  characteristics: string;
+}
+
+const GET_USER = gql`
+  query {
+    Characters{
+    id
+    name
+    age
+    anime
+    characteristics
+  }
+  }
+`;
+
 function App() {
-
-
+  const {data, loading} = useQuery<{Characters: Character[]}>(GET_USER)
+  
+  if(loading) {
+    return <p>Loading...</p>
+  }
+  console.log(data);
+  
   return (
     <>
-      <p>Hello</p>
+    <ul>
+    {data?.Characters.map(Character => <li key={Character.id}>{Character.name}</li>)}
+    </ul>
     </>
   )
 }
